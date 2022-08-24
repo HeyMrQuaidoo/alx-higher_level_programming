555c20f5537f65a42e5cd9bf5a155031b66c3984
HeyMrQuaidoo <codeninja94@gmail.com>
2022-08-23 21:19:47 -0700

    python if

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -2,14 +2,14 @@
 
 /**
  * insert_node - inserts a node in linked list
- * @head: pointer to head of list
- * @number: the number to be added to new node
+ * @head: pointer to head
+ * @number: integer to add to node
  * Return: address of new node
  */
 
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *trav, *new_node;
+	listint_t *arrlist, *new_node;
 
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
@@ -21,17 +21,18 @@ listint_t *insert_node(listint_t **head, int number)
 		new_node->next = NULL;
 		return (new_node);
 	}
-	trav = *head;
-	if (number <= trav->n)
+	
+        arrlist	= *head;
+	if (number <= arrlist->n)
 	{
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
-	while (number >= trav->next->n && trav->next)
+	while (number >= arrlist->next->n && arrlist->next)
 	{
-		trav = trav->next;
-		if (trav->next == NULL && number < trav->n)
+		arrlist = arrlist->next;
+		if (arrlist->next == NULL && number < arrlist->n)
 		{
 			new_node = add_nodeint_end(head, number);
 			if (!new_node)
@@ -39,7 +40,7 @@ listint_t *insert_node(listint_t **head, int number)
 			return (new_node);
 		}
 	}
-	new_node->next = trav->next;
-	trav->next = new_node;
+	new_node->next = arrlist->next;
+	arrlist->next = new_node;
 	return (new_node);
 }
