ace63fc7d337258ef2b436daaa120d78140f1b8e
Code <codeninja94@gmail.com>
2022-08-30 02:21:35 +0000

    update

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    ind = len(my_list) - 1
+    if idx > ind or idx < 0:
+        return my_list
+    my_list[idx] = element
+    
+    return my_list
